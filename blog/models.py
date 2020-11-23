a0585e82ed884fb2bb4cf71fc7f887ca065a04a7
ErrorInever <errorinever@gmail.com>
2020-11-23 02:32:04 +0000

    init post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class StatusPost(models.IntegerChoices):
+	"""Status of post"""
+	DRAFT = 0, 'draft'
+	PUBLISH = 1, 'publish'
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	tagline = models.CharField(max_length=20)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+	content = models.TextField()
+	status = models.IntegerField(default=DRAFT, choices=StatusPost.choices)
+
+	# TODO make author
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		ordering = ['-created_on']
