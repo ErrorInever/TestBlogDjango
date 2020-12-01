62f565de6e2a0145ca8eaea53547ca2a0e62fa2e
ErrorInever <errorinever@gmail.com>
2020-12-01 18:06:45 +0000

    add news agregator

--- core/views.py
+++ core/views.py
@@ -8,3 +8,10 @@ class NewsListView(ListView):
 	model = News
 	template_name = 'news.html'
 	context_object_name = 'list_news'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		last_news = News.objects.filter(status=1).order_by('-created_on')[:3]
+		context['latest_two_news'] = last_news[1:3]
+		context['last_news'] = last_news[0]
+		return context
