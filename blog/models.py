0a63611e383a18089a50127139b2bbdc5c3254f4
ErrorInever <errorinever@gmail.com>
2020-12-06 23:18:52 +0000

    add blog model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Blog(models.Model):
+	blog_title = models.CharField(max_length=50)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.blog_title
 
 
 class StatusPost(models.IntegerChoices):
@@ -14,8 +23,7 @@ class Post(models.Model):
 	updated_on = models.DateTimeField(auto_now=True)
 	content = models.TextField()
 	status = models.IntegerField(default=StatusPost.DRAFT, choices=StatusPost.choices)
-
-	# TODO make author
+	blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
