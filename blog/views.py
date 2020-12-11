2d54bba7ad627b03f2eb35a13857f81dcb94fdce
ErrorInever <errorinever@gmail.com>
2020-12-11 00:01:23 +0000

    init bloglist

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Post
+from .models import Post, Blog
 
 
 class PostListView(ListView):
 	model = Post
 	template_name = 'blog.html'
-	context_object_name = 'list_blog'
+	context_object_name = 'post_list'
